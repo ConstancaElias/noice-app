38028d09f713ef986663f3454bcdadf6f133d90a
Carolina Marques <carolinaresendemarques@ua.pt>
2021-02-27 18:43:06 +0000

    started frontend

--- app-server/public/javascripts/functions.js
+++ app-server/public/javascripts/functions.js
@@ -1,34 +1,42 @@
-$(document).ready(function(){
-  $('.toast').toast('show');
-  
+$("#filterBtn").click(function() {  
+    $("#filterModal").toogle();
 });
 
-$("#filterBtn").click(function(){
-    $("#filterModal").toogle();
-});
-
-$('#uploadSection').click(function () {
+$('#uploadSection').click(function() {
     $('#uploadFiles').show();
 });
 
 $('#ratingSubmit').click(function() {
-    if($('input:radio:checked').length < 1) {
-      $('#alertRadio').show();
-    }
-    else {
-      alert('Success!');
+    if ($('input:radio:checked').length < 1) {
+        $('#alertRadio').show();
+    } else {
+        alert('Success!');
     }
     return false;
-  });
+});
 
-  $(function () {
+$(function() {
     $('.custom-control-input').click(function(e) {
-      $('.custom-control-input').not(this).prop('checked', false);
+        $('.custom-control-input').not(this).prop('checked', false);
     });
-  });
+});
 
-  /*
+/*
   //prevent enter to be submit
   $(document).on("keydown", "form", function(event) { 
     return event.key != "Enter";
 });*/
+
+/* ToDO List */
+$(document).ready(function() {
+    $('button').click(function() {
+        if ($("input[name=task]").val() !== '')
+            $('#todo').append("<ul>" + $("input[name=task]").val() + " <a href='#' class='close' aria-hidden='true'>&times;</a></ul>");
+    });
+    $("body").on('click', '#todo a', function() {
+        $(this).closest("ul").remove();
+    });
+});
+
+
+/*Slider*/
