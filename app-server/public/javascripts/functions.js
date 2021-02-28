1668b040e049c403f154389d5f46e0f3beeed5b3
ConstancaElias <constanca.elias@gmail.com>
2021-02-28 12:33:03 +0000

    Fix timer

--- app-server/public/javascripts/functions.js
+++ app-server/public/javascripts/functions.js
@@ -38,25 +38,70 @@ $(document).ready(function() {
     });
 });
 
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+/* Timer Functions */
+var mySeconds;
+var intervalHandle;
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+function resetPage() {
+    document.getElementById("inputArea").style.display = "none";
 }
 
-window.onload = function () {
-    var fiveMinutes = 60 * 25,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
+function tick() {
+    var timeDisplay = document.getElementById("time");
+
+    var min = Math.floor(mySeconds / 60);
+    var sec = mySeconds - (min * 60);
+
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+
+    var message = min.toString() + ":" + sec;
+
+    timeDisplay.innerHTML = message;
+
+    if (mySeconds === 0) {
+        alert("Done! Great Job!");
+        clearInterval(intervalHandle);
+        resetPage();
+    }
+    mySeconds--;
+
+
+}
+
+function startCounter() {
+    var myInput = document.getElementById("minutes").value;
+    if (isNaN(myInput)) {
+        alert("Type a valid number please");
+        return;
+    }
+    mySeconds = myInput * 60;
+
+    intervalHandle = setInterval(tick, 1000);
+
+    document.getElementById("inputArea").style.display = "none";
+
+
+}
+
+
+window.onload = function() {
+    var myInput = document.createElement("input");
+    myInput.setAttribute("type", "text");
+    myInput.setAttribute("style", "display:table-cell; width:75%");
+    myInput.setAttribute("id", "minutes");
+
+    var myButton = document.createElement("input");
+    myButton.setAttribute("type", "button");
+    myButton.setAttribute("value", "Start Timer");
+
+    myButton.onclick = function() {
+        startCounter();
+
+    }
+    document.getElementById("inputArea").appendChild(myInput);
+    document.getElementById("inputArea").appendChild(myButton);
+
+
+}
